1e9044df9f31ec8d66373ab61821d106980013f2
yeji Jeong <ygjeong55@naver.com>
2024-11-18 16:28:37 +0900

    Login_Error1_yj

---
+++ final_pjt_back/accounts/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path, include
+from . import views
+from .views import UpdateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from dj_rest_auth.views import LoginView, LogoutView
+
+
+app_name='accounts'
+urlpatterns = [
+    # path('login/', views.login, name='login'),
+    path('user/update/', UpdateUserView.as_view(), name='update-user'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 액세스 토큰과 리프레시 토큰 발급
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),  # 리프레시 토큰을 이용한 액세스 토큰 갱신
+    # 회원가입
+    path('accounts/signup/', include('dj_rest_auth.registration.urls')),  # dj_rest_auth의 회원가입 URL 포함
+    # 로그아웃 (JWT 토큰 제거)
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),  # 로그아웃 처리
+]
--- final_pjt_back/accounts/views.py
+++ final_pjt_back/accounts/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from rest_framework import permissions
+# from rest_framework.generics import UpdateAPIView
+# from django.contrib.auth.models import User
+# from django.contrib.auth import get_user_model
+# from .serializers import UserSerializer
+# from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+# @api_view(['POST'])
+# def signup(request):
+#     if request.method == 'POST':
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+# @api_view(['POST'])
+# def login(request):
+#     username = request.data.get('username')
+#     password = request.data.get('password')
+
+#     user = User.objects.filter(username=username).first()
+    
+#     if user and user.check_password(password):  # 비밀번호 검증
+#         refresh = RefreshToken.for_user(user)  # 토큰 생성
+#         access_token = str(refresh.access_token)
+
+#         return Response({
+#             'refresh': str(refresh),
+#             'access': access_token
+#         }, status=status.HTTP_200_OK)
+    
+#     return Response({"detail": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+# class UpdateUserView(UpdateAPIView):
+#     permission_classes = [permissions.IsAuthenticated]
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+#     def get_object(self):
+#         return self.request.user  # 로그인한 사용자의 정보만 수정하도록 설정
+
+#     def perform_update(self, serializer):
+#         # 사용자 정보 저장
+#         serializer.save()
