988056ac85109c4c4e77c56ad0054015afac8d9b
Kangdaekyu <ethan2024@naver.com>
2024-11-19 15:59:12 +0900

    q

--- final_pjt_back/accounts/models.py
+++ final_pjt_back/accounts/models.py
@@ -11,6 +11,6 @@ class User(AbstractUser):
         (2, 'Female'),
     ]
     age = models.IntegerField(null=True)
-    gender = models.IntegerField(choices=GENDER_CHOICES)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=0)
     goal_amount = models.IntegerField(blank=True, null=True)
     products = models.ManyToManyField(FinancialProducts, blank=True)
--- final_pjt_back/accounts/urls.py
+++ final_pjt_back/accounts/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('signup/', include('dj_rest_auth.registration.urls')),
-    path('add-products/', views.add_product)
 ]
