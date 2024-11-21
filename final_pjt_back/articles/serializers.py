77ad287e0d610351f6f4b3cfd15572df94a064c4
yeji Jeong <ygjeong55@naver.com>
2024-11-21 17:33:42 +0900

    Article_Update_Comment_CRUD_Like_yj

--- final_pjt_back/articles/serializers.py
+++ final_pjt_back/articles/serializers.py
@@ -1,71 +1,59 @@
 from rest_framework import serializers
 from .models import Article, Comment, Like, View, ViewCount 
-from accounts.models import User
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-# 유저 정보 조회
 class UserSerializer(serializers.ModelSerializer):
+    """사용자 정보 시리얼라이저"""
     class Meta:
         model = User
-        fields = ('id', 'username', 'age',)
+        fields = ('id', 'username', 'age')
 
-# 게시글 생성 및 조회
-class ArticleSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    
-    class Meta:
-        model = Article
-        fields = '__all__'
-        read_only_fields = ('user',)
-
-# 댓글 생성 및 조회
 class CommentSerializer(serializers.ModelSerializer):
+    """댓글 생성/수정 시리얼라이저"""
     user = UserSerializer(read_only=True)
     
     class Meta:
         model = Comment
-        fields = '__all__'
-        read_only_fields = ('user', 'article')
+        fields = ('id', 'content', 'user', 'article', 'created_at')
+        read_only_fields = ('user', 'article', 'likes')
 
-# 좋아요 생성 및 조회
+class ArticleSerializer(serializers.ModelSerializer):
+    """게시글 생성/수정 시리얼라이저"""
+    user = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+    like_count = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'content', 'user', 'comments', 'created_at', 'like_count', 'is_liked')
+        read_only_fields = ('user', 'comments')
+
+    def get_like_count(self, obj):
+        return obj.like_set.count()
+
+    def get_is_liked(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return obj.like_set.filter(user=request.user).exists()
+        return False
+
 class LikeSerializer(serializers.ModelSerializer):
+    """좋아요 시리얼라이저"""
     class Meta:
         model = Like
         fields = '__all__'
-        read_only_fields = ('user', 'article')
+        read_only_fields = ('user', 'article', 'likes')
 
-# 게시글 목록 조회
-class ArticleListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = ('id', 'title', 'content', 'created_at', 'updated_at')
-
-# 댓글 목록 조회
-class CommentListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ('id', 'content', 'created_at', 'updated_at')
-
-# 좋아요 목록 조회
-class LikeListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Like
-        fields = ('id', 'created_at', 'updated_at')
-
-# 게시글 상세 조회
 class ArticleDetailSerializer(serializers.ModelSerializer):
-    comment_set = CommentListSerializer(many=True, read_only=True)
-    like_set = LikeListSerializer(many=True, read_only=True)
+    """게시글 상세 시리얼라이저"""
+    user = UserSerializer(read_only=True)
+    comment_set = CommentSerializer(many=True, read_only=True)
+    like_set = LikeSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Article
         fields = '__all__'
-        read_only_fields = ('user',)
-
-# 조회수 목록 조회  
-class ViewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = View
-        fields = '__all__'
-        read_only_fields = ('user',)    
+        read_only_fields = ('user', 'likes')
--- final_pjt_back/articles/urls.py
+++ final_pjt_back/articles/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.article_list),                                                   # 게시글 목록 조회
-    path('<int:article_pk>/', views.article_detail),                                # 게시글 상세 조회 및 수정 및 삭제
-    # path('<int:article_pk>/comments/', views.comment_list),                         # 댓글 목록 조회 및 생성
-    # path('<int:article_pk>/comments/<int:comment_pk>/', views.comment_detail),      # 댓글 상세 조회 및 수정 및 삭제
+    path('', views.article_list, name='article_list'),                                # 게시글 목록 조회
+    path('<int:article_pk>/', views.article_detail, name='article_detail'),           # 게시글 상세 조회 및 수정 및 삭제
+    path('<int:article_pk>/comments/', views.comment_list, name='comment_list'),      # 댓글 목록 조회 및 생성
+    path('<int:article_pk>/comments/<int:comment_pk>/', views.comment_detail, name='comment_detail'), # 댓글 상세 조회 및 수정 및 삭제
+    path('<int:article_pk>/like/', views.article_like, name='article_like'),          # 게시글 좋아요 생성 및 삭제
     # path('<int:article_pk>/likes/', views.like_list),                               # 좋아요 목록 조회 및 생성
     # path('<int:article_pk>/likes/<int:like_pk>/', views.like_detail),               # 좋아요 상세 조회 및 수정 및 삭제
-    # path('<int:article_pk>/like/', views.article_like),                             # 게시글 좋아요 생성 및 삭제
     # path('<int:article_pk>/comments/<int:comment_pk>/like/', views.comment_like),   # 댓글 좋아요 생성 및 삭제
+   
 ]
