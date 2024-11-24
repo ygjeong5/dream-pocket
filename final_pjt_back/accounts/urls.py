148e4b68808f5513b1773484ba0b62ecf160012d
Kangdaekyu <ethan2024@naver.com>
2024-11-24 16:48:18 +0900

    ledger

--- final_pjt_back/accounts/urls.py
+++ final_pjt_back/accounts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import LedgerViewSet
+
+router = DefaultRouter()
+router.register(r'ledgers', LedgerViewSet, basename='ledger')
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
     path('signup/', include('dj_rest_auth.registration.urls')),
     path('user-info/', views.user_info),
+    path('ledgers/', include(router.urls)),  # Router에 정의된 URL 포함
 ]
--- final_pjt_back/accounts/views.py
+++ final_pjt_back/accounts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.shortcuts import get_list_or_404, get_object_or_404
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Ledger
+from .serializers import UserSerializer, LedgerSerializer
 from django.http import JsonResponse
 import requests
-from .serializers import UserSerializer
 
 @api_view(['GET', 'PUT'])
 @permission_classes([IsAuthenticated])
@@ -24,3 +25,15 @@ def user_info(request):
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
+
+class LedgerViewSet(viewsets.ModelViewSet):
+    queryset = Ledger.objects.all()
+    serializer_class = LedgerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
